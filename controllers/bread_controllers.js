ea2f1bcfd56b3c2b91336092de2e5982f1da46fd
Campbell1105 <135083465+Campbell1105@users.noreply.github.com>
2023-08-20 14:38:57 -0400

    added jsx files

--- controllers/bread_controllers.js
+++ controllers/bread_controllers.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const breads = express.Router()
+const Bread = require('../models/bread.js')
 
 // Index
 breads.get('/', (req, res) => {
-res.send('this is the index at /breads')
+//res.send(Bread)
+res.render('index')
+})
+
+breads.get('/:arrayIndex', (req, res) =>{
+    res.send(bread[req.params.arrayIndex])
 })
 
 module.export = breads
