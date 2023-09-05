f5cfe0ce324af364e0a5c5975c1695e6b4dc5a57
Campbell1105 <135083465+Campbell1105@users.noreply.github.com>
2023-09-05 19:03:18 -0400

    create baker model and seed

---
+++ models/baker.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const bakerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+        },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    bio: {
+        type: String,
+    },
+})
+
+const Baker = mongoose.model('baker', bakerSchema)
+module.exports = Baker
